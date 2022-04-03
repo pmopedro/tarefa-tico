90dec6c8d602500de3f4431827fd18f1f0fea274
Pedro M <p.mendes.odilon@gmail.com>
2022-04-03 13:35:12 -0300

    feature implements ticos task 2

---
+++ vetor2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Crie um programa em c# que leia um vetor de 10 alturas e
+// elabore um algoritmo que permita calcular:
+// A média das alturas
+
+double[] entrada = new double[10];
+double media=0;
+
+Console.WriteLine("Entre com 10 valores");
+for (int i=0; i < 10; i++){
+    entrada[i] = Convert.ToInt32(Console.ReadLine());
+    media = entrada[i]/10 + media;
+    Console.WriteLine("A parcela da média é: " + entrada[i]/10);
+    Console.WriteLine("Escreva mais " + (9-i));
+};
+
+Console.WriteLine("Sua média é " + media+" valores");
---
+++ vetor2/vetor1/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Crie um programa em c# que leia um vetor de 10
+// números inteiros e gere um segundo vetor (que deve ser
+// impresso), cujas posições pares são o dobro do vetor
+// original e as ímpares são o triplo.
+
+
+int[] entrada = new int[10];
+int[] saida = new int[10];
+
+Console.WriteLine("Entre com 10 valores");
+for (int i=0; i < 10; i++){
+    entrada[i] = Convert.ToInt32(Console.ReadLine());
+};
+
+for (int i=0; i < 10; i++){
+    if (i%2 == 1){ //quando ele é impar!! 
+        saida[i]=entrada[i]*3;
+    }else{ // caso contrário (caso par)
+        saida[i]=entrada[i] *2;
+    }
+};
+
+Console.WriteLine("Os valores entrantes foram:");
+for (int i=0; i < 10; i++) {
+    Console.WriteLine(saida[i]);
+};
